8ce973edf868f50607543e2e8c9663f74cd20d16
enkuso <eegii2you@yahoo.com>
2016-10-08 11:38:13 +0900

    ene aylal deer udriig ni ingej tawiya http://www.travelallmongolia.com/tour/a-classic-journey-of-mongolia-with-naadam-festival.html#.V_g9f_l97IU June 29 to July 13, 2017 July 9-23, 2017

--- src/Enkuso/Content/Tours.php
+++ src/Enkuso/Content/Tours.php
@@ -42,7 +42,7 @@ class Tours
                 'https://farm4.staticflickr.com/3852/15205063450_059a00292b_z.jpg',
             ),
             'region' => 'Central, Southern and Northern part of Mongolia.',
-            'availableTime' => '<br/>&nbsp;&nbsp;<a href="/uploads/tour-pdf/A Classic Journey Of Mongolia With Rural Naadam Festival In Karakorum 15 Days.pdf">A Classic Journey of Mongolia with Rural Naadam Festival July 5-19</a>, <br/>&nbsp;&nbsp;9th - 23th of July, <br/>&nbsp;&nbsp;29th of June – 13th of July',
+            'availableTime' => '<br/>&nbsp;&nbsp;June 29 to July 13, 2017<br/>&nbsp;&nbsp; July 9-23, 2017',
             'map' => 'https://farm4.staticflickr.com/3872/15368668506_1bf7ecaabb_m.jpg',
             'mapLarge' => 'https://farm4.staticflickr.com/3872/15368668506_8ca8785279_k.jpg',
             'bs_img' => '/img/tour/jeep_main.jpg',
