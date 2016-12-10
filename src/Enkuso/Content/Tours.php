97720d1366ab197827434f69b13ff0088214f612
enkuso <eegii2you@yahoo.com>
2016-12-10 15:11:33 +0900

    fix

--- src/Enkuso/Content/Tours.php
+++ src/Enkuso/Content/Tours.php
@@ -1031,7 +1031,7 @@ You will enjoy a stay at a hot spring resort for two days where you can soak in 
                 '/tourimg/golden-eagle/eagle (5).jpg',
             ),
             'region' => 'Western part of Mongolia.',
-            'availableTime' => 'Sep 14-21 & Sep 28 - Oct 12, 2017',
+            'availableTime' => 'Sep 14-21 & Sep 28 - Oct 5, 2017',
             'map' => 'https://farm6.staticflickr.com/5642/22199130788_18efaf4b22_m.jpg',
             'mapLarge' => 'https://farm6.staticflickr.com/5642/22199130788_18efaf4b22_b.jpg',
             'bs_img' => '/img/tour/jeep_main.jpg',
