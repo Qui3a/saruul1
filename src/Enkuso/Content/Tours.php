8872d60e37b168f9af2bff7b1eea4533d8a9f2bf
enkuso <eegii2you@yahoo.com>
2017-03-06 23:33:23 +0800

    eagle-festival-and-gobi-desert

--- src/Enkuso/Content/Tours.php
+++ src/Enkuso/Content/Tours.php
@@ -991,7 +991,7 @@ You will enjoy a stay at a hot spring resort for two days where you can soak in 
                 '/tourimg/golden-eagle-and-gobi/eagle-festival (7).jpg',
             ),
             'region' => 'Western & Southern part of Mongolia.',
-            'availableTime' => 'Sep 21 - Oct 4, 2017',
+            'availableTime' => 'Sep 7-20 & Sep 21 - Oct 4, 2017',
             'map' => 'https://farm6.staticflickr.com/5635/22173683418_7f809bf618_m.jpg',
             'mapLarge' => 'https://farm6.staticflickr.com/5635/22173683418_7f809bf618_b.jpg',
             'bs_img' => '/img/tour/jeep_main.jpg',
